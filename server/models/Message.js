2f0cd3847d09b66fe507192cf214190da3384289
Marta Jopp <martapeterson@gmail.com>
2018-03-16 21:23:48 -0500

    new schema

--- server/models/Message.js
+++ server/models/Message.js
@@ -17,6 +17,19 @@ const MessageSchema = new Schema({
 //could create an empty array and push userIDs of votes into - loop through
 //each time and check if that user has already voted?
 //or each vote type can be it's own object with properties of a number of votes
-//and also a list of who has voted and is returned in the get request
+//and also a list of who has voted and is returned in the get request:
+//heart: { 
+//        votes: {type: Number, default: 0},
+//        byWho: {type: Array}
+//}
+// OR
+// create a schemas inside of the message schema, so 
+//var HeartSchema = new Schema({
+// id: {type: String, required: true, index: {unique: true}},
+// votes: { type: Number},
+// whovoted: {type: array}
+// });
+//and then above it would be heart: [HeartSchema]
+// var Heart = mongoose.model('Hearts', HeartSchema);
 
 module.exports = mongoose.model('Message', MessageSchema);
